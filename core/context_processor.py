2c2a2af5d5dabc95eb8764d0128aba102c61553c
Peter Mbachu <petermbachu.bincom@gmail.com>
2023-09-25 15:56:18 +0000

    Delete context_processor.py

--- core/context_processor.py
+++
@@ -1,9 +0,0 @@
-from .models import UserCredential
-
-
-def aside_bar(request):
-    try:
-        user_credential = UserCredential.objects.get(user=request.user.id)
-    except UserCredential.DoesNotExist:
-        user_credential = None
-    return {'user_credential': user_credential}
