4fa96e4afb295b29baaccbcad7a1485d5bae312b
Peter Mbachu <petermbachu.bincom@gmail.com>
2023-11-02 15:03:22 +0000

    added password url paths in urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import (
     ZoomVideoListView, AdminLoginView, AdminLogoutView,
-    GetZoomVideoView, ZoomVideoDetailView, UploadDetailView
+    GetZoomVideoView, ZoomVideoDetailView, UploadDetailView,
+    AdminPasswordResetView, AdminPasswordResetDoneView,
+    AdminPasswordResetConfirmView, AdminPasswordResetCompleteView
 )
 
 app_name = 'main'
@@ -13,4 +15,10 @@ urlpatterns = [
     path('<int:pk>/zoom/', GetZoomVideoView.as_view(), name='user_zoom'),
     path('detail/<slug:slug>/', ZoomVideoDetailView.as_view(), name='detail'),
     path('<int:pk>/<slug:slug>/upload/', UploadDetailView.as_view(), name='upload'),
+    path('password-reset/', AdminPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', AdminPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', AdminPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/success/', AdminPasswordResetCompleteView.as_view(),
+         name='password_reset_complete')
 ]
