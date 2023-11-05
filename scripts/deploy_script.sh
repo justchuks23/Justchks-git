6947d80294c21f2719fe35d44fc134b9e9dd536a
James Chukwu <jameschukwu.bincom@gmail.com>
2023-11-05 15:34:18 +0000

    Add new file

---
+++ scripts/deploy_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x  # Enable debugging
+
+sudo docker load --input zoom_youtube_integration.tar
+
+#####################################################################################################################
+sudo docker stop $ZOOM_YOUTUBE_INTEGRATION_CONTAINER && sudo docker rm $ZOOM_YOUTUBE_INTEGRATION_CONTAINER
+sudo docker system prune -f
+sudo docker volume prune -f
+######################################################################################################################
+docker-compose -f docker-compose.yml up -d --build
