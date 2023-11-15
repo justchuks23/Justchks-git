6edc5205cc3f587dc641972b72c03929ce7f3cec
Peter Mbachu <petermbachu.bincom@gmail.com>
2023-11-15 23:35:33 +0000

    Update deploy_script.sh

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -4,7 +4,7 @@ set -x  # Enable debugging
 sudo docker load --input zoom_youtube_integration.tar
 
 #####################################################################################################################
-sudo docker stop $ZOOM_YOUTUBE_INTEGRATION_CONTAINER && sudo docker rm $ZOOM_YOUTUBE_INTEGRATION_CONTAINER
+sudo docker stop $Z2Y_CONTAINER && sudo docker rm $Z2Y_CONTAINER
 sudo docker system prune -f
 sudo docker volume prune -f
 ######################################################################################################################
