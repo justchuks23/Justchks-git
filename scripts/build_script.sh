c3b828322029d9b1272934ed7d3428bf7ffba706
James Chukwu <jameschukwu.bincom@gmail.com>
2023-11-15 22:27:41 +0000

    Update scripts/build_script.sh

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -10,9 +10,9 @@ cat .env
 docker build -t $ZOOM_YOUTUBE_INTEGRATION_IMAGE .
 docker save $ZOOM_YOUTUBE_INTEGRATION_IMAGE > zoom_youtube_integration.tar
 
-sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile || true"
+sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile docker-compose.yml || true"
 
-sshpass -p $SERVER_PASSWORD scp -r -o StrictHostKeyChecking=no zoom_youtube_integration.tar Dockerfile scripts/deploy_script.sh $SERVER_USER@$SERVER_IP:~/
+sshpass -p $SERVER_PASSWORD scp -r -o StrictHostKeyChecking=no zoom_youtube_integration.tar docker-compose.yml Dockerfile scripts/deploy_script.sh $SERVER_USER@$SERVER_IP:~/
 
 
 
