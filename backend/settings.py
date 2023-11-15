2cfa587a35f0d2aac91791707832d12fd20fe1df
James Chukwu <jameschukwu.bincom@gmail.com>
2023-11-15 21:39:42 +0000

    Update docker-compose.yml, backend/settings.py, scripts/build_script.sh, .gitlab-ci.yml files

--- backend/settings.py
+++ backend/settings.py
@@ -115,23 +115,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR/'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': 5432,
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR/'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': config('DB_NAME'),
+    #    'USER': config('DB_USER'),
+     #   'PASSWORD': config('DB_PASSWORD'),
+      #  'HOST': config('DB_HOST'),
+       # 'PORT': 5432,
+   # }
+#}
 
 
 # Password validation
--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,7 +1,7 @@
-echo "DB_HOST=$DB_HOST" >> .env
-echo "DB_NAME=$DB_NAME" >> .env
-echo "DB_USER=$DB_USER" >> .env
-echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+#echo "DB_HOST=$DB_HOST" >> .env
+#echo "DB_NAME=$DB_NAME" >> .env
+#echo "DB_USER=$DB_USER" >> .env
+#echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "SECRET_KEY=$SECRET_KEY" >> .env
 #sleep 50
 ls -la
