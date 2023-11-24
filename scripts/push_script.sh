adcc4cc8d9dc62d8893dd0492e4a5c189c56fb8c
James Chukwu <jameschukwu.bincom@gmail.com>
2023-11-24 08:35:20 +0000

    Update scripts/build_script.sh, scripts/push_script.sh files

--- scripts/push_script.sh
+++ scripts/push_script.sh
@@ -1,6 +1,6 @@
 #sshpass -p $SERVER_PASSWORD scp -r -o StrictHostKeyChecking=no zoom_youtube_integration.tar $SERVER_USER@$SERVER_IP:~/
 
-sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile docker-compose.yml || true"
+#sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile docker-compose.yml || true"
 
 sshpass -p $SERVER_PASSWORD scp -r -o StrictHostKeyChecking=no zoom_youtube_integration.tar Dockerfile docker-compose.yml scripts/deploy_script.sh $SERVER_USER@$SERVER_IP:~/
 
