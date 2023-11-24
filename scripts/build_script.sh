9003926902799b7a6762cd6282c80500b59fe5c6
James Chukwu <jameschukwu.bincom@gmail.com>
2023-11-24 08:07:26 +0000

    Update scripts/push_script.sh, scripts/build_script.sh, .gitlab-ci.yml files

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -10,9 +10,9 @@ cat .env
 docker build -t $Z2Y_IMAGE .
 docker save $Z2Y_IMAGE > zoom_youtube_integration.tar
 
-sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile docker-compose.yml || true"
+#sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile docker-compose.yml || true"
 
-sshpass -p $SERVER_PASSWORD scp -r -o StrictHostKeyChecking=no zoom_youtube_integration.tar Dockerfile docker-compose.yml scripts/deploy_script.sh $SERVER_USER@$SERVER_IP:~/
+#sshpass -p $SERVER_PASSWORD scp -r -o StrictHostKeyChecking=no zoom_youtube_integration.tar Dockerfile docker-compose.yml scripts/deploy_script.sh $SERVER_USER@$SERVER_IP:~/
 
 
 
