26f2ffc17885d331762d70f67ed0e7dea25b054e
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2024-07-17 16:25:32 +0100

    added more custom form

--- core/forms.py
+++ core/forms.py
@@ -7,10 +7,26 @@ class AdminLoginForm(AuthenticationForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserCrendentialAdminForm(forms.ModelForm):
-    user_credentials = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    zoom_email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'})
     )
-    
-    class Meta: 
+    zoom_client_id = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    zoom_client_secret = forms.CharField(widget=forms.PasswordInput(attrs={'classs- form-control'})
+    )
+    zoom_account_id = forms.CharField(widget=forms.PasswordInput(attrs={'classs- form-control'})
+    )
+    google_email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    google_client_id = forms.CharField(widget=forms.PasswordInput(attrs={'classs- form-control'}) 
+    )
+    google_client_secret = forms.CharField(widget=forms.PasswordInput(attrs={'classs- form-control'})
+    )  
+    google_code = forms.CharField(widget=forms.PasswordInput(attrs={'classs- form-control'})    
+    )
+    google_refresh_token = forms.CharField(widget=forms.PasswordInput(attrs={'classs- form-control'})  
+    )                      
+    class Meta:
         model = UserCredential
         fields=['zoom_client_id', 'zoom_client_secret',
                 'zoom_account_id', 'google_client_id', 'google_client_secret',
