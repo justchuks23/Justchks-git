db251db80cfc43fbe22fa430c4d9c252e70ec987
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2024-07-17 07:11:33 +0100

    added custom form

--- core/forms.py
+++ core/forms.py
@@ -6,22 +6,22 @@ class AdminLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+class UserCrendentialAdminForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    
+    class Meta: 
+        model = UserCredential
+        fields=['zoom_client_id', 'zoom_client_secret',
+                'zoom_account_id', 'google_client_id', 'google_client_secret',
+                'google_code', 'google_refresh_token']
+
 
 class ZoomYoutubeUploadForm(forms.Form):
     zoom_id = forms.CharField(
         widget=forms.TextInput(attrs={'class': 'form-control'}),
         label='Zoom ID'
     )
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-input'}))
-
-class UserCrendentialFileForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(UserCrendentialFileForm, self).__init__(args, **kwargs)
-        self.fields['zoom_client_id', 'zoom_client_secret']
-
-    class Meta: 
-        model = UserCredential
-        exclude = ('slug',)
 
 class UploadYoutubeForm(forms.Form):
     user_id = forms.IntegerField()
