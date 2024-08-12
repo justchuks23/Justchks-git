9d6a5d225db054511bac5971aad9993f09e9b5b3
James Chukwu <jameschukwu.bincom@gmail.com>
2024-08-12 13:40:04 +0000

    Update scripts/build_script.sh, Dockerfile files

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -8,7 +8,6 @@ ls -la
 cat .env 
 
 docker build -t $Z2Y_IMAGE .
-/home/james_chukwu/zoom-youtube-integration/
 docker save $Z2Y_IMAGE > zoom_youtube_integration.tar
 
 #sshpass -p $SERVER_PASSWORD ssh -tt -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP "echo '$SERVER_PASSWORD' | rm deploy_script.sh  zoom_youtube_integration.tar Dockerfile docker-compose.yml || true"
