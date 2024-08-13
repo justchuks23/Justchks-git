bd17589c9eadbfaa4075aa6bf4ed6b7ebd7ec6ac
James Chukwu <jameschukwu.bincom@gmail.com>
2024-08-13 11:28:57 +0000

    Update scripts/build_script.sh

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
 
-# Print the current directory
-echo "Current directory:"
-pwd
-
-# List files in the build context
-echo "Listing files in the build context:"
-ls -la
-
-# Check if site_conf.conf exists
-if [ -f "site_conf.conf" ]; then
-    echo "site_conf.conf file exists."
-else
-    echo "site_conf.conf file does not exist."
-fi
 echo "SECRET_KEY=$SECRET_KEY" >> .env
 echo "DEBUG=$DEBUG" >> .env
 echo "DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME" >> .env
