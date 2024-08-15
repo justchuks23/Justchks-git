6ec6a13a84d68cfb0efa4320dc6c47eee1ad48d5
James Chukwu <jameschukwu.bincom@gmail.com>
2024-08-15 13:21:41 +0000

    Update scripts/deploy_script.sh

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -8,4 +8,5 @@ sudo docker stop $Z2Y_CONTAINER && sudo docker rm $Z2Y_CONTAINER
 sudo docker system prune -f
 sudo docker volume prune -f
 ################################################################################################################
+ls -la
 docker-compose -f docker-compose.yml up -d 
