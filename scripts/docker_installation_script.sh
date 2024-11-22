20a32ef5c90ecaaed6a5fc563b9b0a1cd7472142
James Chukwu <jameschukwu.bincom@gmail.com>
2024-11-22 13:20:46 +0000

    Add new file

---
+++ scripts/docker_installation_script.sh
@@ -0,0 +1,8 @@
+################# Docker Installation ######################################################
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo docker -v
+sudo systemctl enable --now docker
+sudo apt-get install openssh-client 
+################## Installation of Docker-Compose #########################################
+sudo apt  install docker-compose -y
