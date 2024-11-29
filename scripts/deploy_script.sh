c2713e0b01a60817e384073cdf2751a93ac916cb
James Chukwu <jameschukwu.bincom@gmail.com>
2024-11-29 13:39:31 +0000

    Update scripts/deploy_script.sh

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -7,5 +7,5 @@ sudo docker load --input zoom_youtube_integration.tar
 sudo docker stop $Z2Y_CONTAINER && sudo docker rm $Z2Y_CONTAINER
 sudo docker system prune -f
 sudo docker volume prune -f
-#############################################################################################################
+############################################################################################################
 docker-compose -f docker-compose.yml up -d 
