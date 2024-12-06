4f5e57da21e95ecd7ef56b8806f20eaa1c2b2330
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2024-12-06 11:25:35 +0100

    remove ci/cd configuration and script directory

--- scripts/docker_installation_script.sh
+++
@@ -1,8 +0,0 @@
-################# Docker Installation ######################################################
-sudo apt-get update -y
-sudo apt-get install docker.io -y
-sudo docker -v
-sudo systemctl enable --now docker
-sudo apt-get install openssh-client 
-################## Installation of Docker-Compose #########################################
-sudo apt  install docker-compose -y
