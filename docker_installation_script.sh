c1adfc5374f4ba3cdfa517e21b1d30ced21415f2
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2024-12-06 14:11:13 +0100

    ci/cd script

--- docker_installation_script.sh
+++ docker_installation_script.sh
@@ -6,4 +6,3 @@ sudo systemctl enable --now docker
 sudo apt-get install openssh-client 
 ################## Installation of Docker-Compose #########################################
 sudo apt  install docker-compose -y
-#
