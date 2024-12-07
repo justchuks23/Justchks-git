dbedb6db79bcb2134610759a059a5a8d5f1d06e2
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2024-12-07 14:49:03 +0100

    check b4_script

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -2,7 +2,7 @@ touch ~/.ssh/id_rsa
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host *\nStrictHostKeyChecking no\n" > ~/.ssh/config
-eval "$(ssh_agent -s)"
+eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 chmod 700 ~/.ssh
 
